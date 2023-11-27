2a0366e736462ced95e40d0ba2c12323d207aa33
Liam Peachey <liamjpeachey@gmail.com>
2023-11-27 16:31:29 -0600

    Wrote error page, completed contact page functionality (slight bug in email verification).

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,63 @@
+import { useState } from "react";
 export default function ContactPage() {
+    const [missingFields, setMissingFields] = useState([]);
+    const [emailError, setEmailError] = useState(true);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!name || !email || !message || emailError) {
+            console.log('here');
+            return;
+        }
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
+    const handleBlur = (e) => {
+        e.preventDefault();
+        if (e.target.value == "") {
+            setMissingFields([...missingFields, e.target.id]);
+        }
+        else if (missingFields.includes(e.target.id)) {
+            setMissingFields(missingFields.filter((id) => id != e.target.id));
+        }
+        console.log('blurred');
+    };
+    const handleChange = (e) => {
+        switch (e.target.id) {
+            case 'name':
+                setName(e.target.value)
+                break;
+            case 'email':
+                setEmail(e.target.value)
+                setEmailError(email.match(/^([\w\+~-]+)@([\w]+)\.([a-z]{2,63})$/));
+                break;
+            case 'message':
+                setMessage(e.target.value)
+                break;
+            default:
+                break;
+        }
+    }
+    const handleInvalid = (e) => {
+
+    };
     return (
         <div>
-
+            <form className="" onSubmit={handleSubmit}>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" onBlur={handleBlur} onChange={handleChange} value={name} />
+                <label htmlFor="email">Email Address:</label>
+                <input type="email" id="email" onBlur={handleBlur} onChange={handleChange} value={email} />
+                <label htmlFor="message">Message:</label>
+                <textarea name="email-body" id="message" cols="30" rows="10" onBlur={handleBlur} onChange={handleChange} value={message}></textarea>
+                {missingFields.length ? (<p>Fields {missingFields.join(', ')} must be filled in.</p>) : (<></>)}
+                {!emailError ? (<p>Please enter a valid email!</p>) : (<></>)}
+                <button>Submit</button>
+            </form>
         </div>
     );
 }
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,15 @@
-export default function ErrorPage() {
-    return (
-        <div>
+import { useRouteError } from 'react-router-dom';
 
+export default function ErrorPage() {
+    const error = useRouteError();
+    console.error(error);
+    return (
+        <div id="error-page">
+            <h1>So Sorry!</h1>
+            <p>An unexpected error has occurred.</p>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
         </div>
     );
 }
