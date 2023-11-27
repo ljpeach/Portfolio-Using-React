ea74c2e46e9787ca8604cd9533968400d8cea028
Liam Peachey <liamjpeachey@gmail.com>
2023-11-27 15:41:16 -0600

    Added alt text to portrait, added links to footer. Footer still needs icons for each link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,18 @@
+import { Link } from 'react-router-dom';
 export default function Footer() {
-    return (<></>);
+    return (
+        <footer>
+            <ul>
+                <Link key={1} to='https://github.com/ljpeach' target='_blank'>
+                    <img src="#" alt="GitHub" />
+                </Link>
+                <Link key={2} to='https://www.linkedin.com/in/liam-peachey/' target='_blank'>
+                    <img src="#" alt="LinkedIn" />
+                </Link>
+                <Link key={3} to='mailto:liamjpeachey@gmail.com' target='_blank'>
+                    <img src="#" alt="Email" />
+                </Link>
+            </ul>
+        </footer>
+    );
 }
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
     return (
         <section>
             <h2>About Liam Peachey</h2>
-            <img src="./src/assets/images/portrait.jpg" alt="" />
+            <img src="./src/assets/images/portrait.jpg" alt="Portrait of Liam" />
             <p>
                 Liam Peachey is an aspiring software developer with a degree in computer science and Japanese language and linguistics, currently working on a bootcamp certification. Their interest in computer science was piqued in their senior year of high school, and has burned strong in the years since. Working as a tutor for Earlham College's computer science department, they strove to foster a similar passion in others. In their free time, they enjoy playing video games, watching video essays, biking or rollerblading, and reading. They would like to add knitting to their repertoire as well.
             </p>
